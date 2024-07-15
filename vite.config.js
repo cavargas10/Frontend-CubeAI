53762ba2a3a18de7827ca5304014c0a08b973fad
Carlos Vargas R. <carvarjr_95@hotmail.com>
2024-07-15 05:26:25 -0500

    Cambios en Cinfig Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: './',
+  assetsInclude: ['**/*.hdr'],
+  resolve: {
+    alias: {
+      'three': 'three',
+    },
+  },
+  optimizeDeps: {
+    include: ['three'],
+  },
 })
